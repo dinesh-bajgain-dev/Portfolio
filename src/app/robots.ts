6b404d0d1aaab395add346148dc74a210d47c48f
dinesh-bazgain <dinesh.bazgain@gmail.com>
2026-04-02 19:17:34 +0545

    fix(robots): remove '/scripts/' from disallowed paths in robots.txt

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -9,7 +9,7 @@ export default function robots(): MetadataRoute.Robots {
       {
         userAgent: "*",
         allow: "/",
-        disallow: ["/api/", "/private/", "/scripts/", "/mark.png"],
+        disallow: ["/api/", "/private/", "/mark.png"],
       },
     ],
     sitemap: `${baseUrl}/sitemap.xml`,
